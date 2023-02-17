9963a23159fc4b7b99a17c966deb117fc809f2f7
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-02-17 09:52:01 +0530

    Added dependencies for MLkit with Flutter, added Painter files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +21,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  NavigationBarWidget()
+      home:  NavigationBarWidget(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
