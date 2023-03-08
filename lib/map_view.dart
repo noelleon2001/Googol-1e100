fb5cc70511791d4114221e792784f1101b302cd8
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2023-03-08 22:08:35 +0800

    Added functionality for displaying markers

--- lib/map_view.dart
+++ lib/map_view.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -13,16 +16,18 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   late GoogleMapController mapController;
 
-  LatLng? currentLatLng = const LatLng(3.0639, 101.6007);
+  LatLng? currentLatLng;
   Location location = Location();
   LocationData? _currentLocation;
+
+  Set<Marker> markers = {};
 
 
   @override
   void initState() {
     super.initState();
-    // getLocation();
-    // findPlaces();
+    getLocation();
+    findPlaces();
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -38,6 +43,8 @@ class _MapViewState extends State<MapView> {
         zoom: 11.0,
       ),
       myLocationEnabled: true,
+      zoomGesturesEnabled: true,
+      markers:markers,
     );
   }
 
@@ -84,9 +91,89 @@ class _MapViewState extends State<MapView> {
     super.dispose();
   }
 
+
+  /*
+  * api request -> json parse -> iterate -> place markers (use futurebuilder)
+  * */
   void findPlaces() async{
-    var url = Uri.https('maps.googleapis.com', 'maps/api/place/findplacefromtext/json', {'input': 'Recycle', 'inputtype':'textquery', 'key': 'API_KEY'});
+    var url = Uri.https('maps.googleapis.com', 'maps/api/place/findplacefromtext/json',
+        {'input': 'recycle',
+        'inputtype':'textquery',
+          'key': dotenv.env['GMAP_KEY'],
+          'fields': 'formatted_address,geometry,name'});
     var response = await http.post(url);
-    print('Response.body: ${response.body}');
+
+    Set<Marker> markers = <Marker>{};
+
+    if (response.statusCode == 200){
+      MapResponse mapResponse = MapResponse.fromJSON(jsonDecode(response.body));
+      print('MapResponse: ${mapResponse.status}');
+
+      mapResponse.places.forEach((place) => {
+        markers.add(Marker(
+            markerId: MarkerId(place.name),
+            position: place.latLng,
+            infoWindow: InfoWindow(
+                title: place.name,
+                snippet: place.formattedAddress
+            )))
+      });
+
+    }
+    setState(() => {
+      this.markers = markers
+    });
+  }
+}
+
+class MapResponse{
+  final List<Place> places;
+  final String status;
+
+  const MapResponse({
+    required this.places,
+    required this.status
+  });
+
+  factory MapResponse.fromJSON(Map<String, dynamic> json){
+
+    // candidates format: [{'formatted_address': '', ...}]
+    final placesData = json['candidates'] as List<dynamic>;
+
+    final places = placesData
+        .map((place) => Place.fromJson(place))
+        .toList();
+
+    return MapResponse(
+        places: places,
+        status: json['status'] as String
+    );
+  }
+}
+
+
+class Place{
+  final String formattedAddress;
+  final String name;
+  // final String location;
+  final LatLng latLng;
+
+  const Place({
+    required this.formattedAddress,
+    required this.name,
+    required this.latLng
+  });
+
+  factory Place.fromJson(Map<String, dynamic> json){
+    print(json);
+     var location = json['geometry']['location'] as Map<String, dynamic>,
+         lat = location['lat'],
+         lng = location['lng'];
+
+    return Place(
+      formattedAddress: json['formatted_address'],
+      latLng: LatLng(lat!, lng!),
+      name: json['name']
+    );
   }
 }
